5f099a1290f5f03efbb02bc9d5efe411b1683033
Kyle Harrington <janelia@kyleharrington.com>
2020-02-03 12:38:03 -0500

    Add code for caching mean value when saving

--- src/main/java/org/janelia/saalfeldlab/hotknife/SubsampleHeightmap.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/SubsampleHeightmap.java
@@ -2,6 +2,8 @@ package org.janelia.saalfeldlab.hotknife;
 
 import ch.systemsx.cisd.hdf5.HDF5Factory;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
+import mpicbg.util.RealSum;
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.cache.img.CachedCellImg;
 import net.imglib2.converter.Converters;
@@ -62,6 +64,31 @@ public class SubsampleHeightmap {
         // Using an HDF5 RAI can be slow when computing transforms, write to N5 and use that
         N5FSWriter n5w = new N5FSWriter(n5Path);
         N5Utils.save(minConv, n5w, outDataset, new int[]{1024, 1024}, new RawCompression(), exec);
+
+        DoubleType minMean = getAvgValue(minConv);
+        n5w.setAttribute(outDataset, "mean", minMean.getRealDouble());
+    }
+
+    public static DoubleType getAvgValue(RandomAccessibleInterval<DoubleType> rai) {
+        RandomAccess<DoubleType> ra = rai.randomAccess();
+        long[] pos = new long[rai.numDimensions()];
+        for( int k = 0; k < pos.length; k++ ) pos[k] = 0;
+        ra.localize(pos);
+
+        long count = 0;
+        final RealSum sum = new RealSum();
+        for( pos[0] = 0; pos[0] < rai.dimension(0); pos[0]++ ) {
+            for( pos[1] = 0; pos[1] < rai.dimension(1); pos[1]++ ) {
+                ra.setPosition(pos);
+                final double v = ra.get().getRealDouble();
+                if( !Double.isNaN(v) && !Double.isInfinite(v) ) {
+                	sum.add( v );
+                    count++;
+                }
+            }
+        }
+
+        return new DoubleType(sum.getSum() / count);
     }
 
 }
