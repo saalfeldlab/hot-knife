157420a55c263975d332dc14a7f0564746136274
Stephan Preibisch <stephan.preibisch@gmx.de>
2020-07-07 16:08:21 -0400

    bit more debug output

--- src/main/java/org/janelia/saalfeldlab/hotknife/ViewAlignment.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/ViewAlignment.java
@@ -114,6 +114,7 @@ public class ViewAlignment {
 		final int showScaleIndex = options.getScaleIndex();
 		final double showScale = 1.0 / (1 << showScaleIndex);
 
+		System.out.println( "showscale: " + showScale );
 		Bdv bdv = null;
 
 		final int numProc = Math.max( 1, Runtime.getRuntime().availableProcessors() / 2 );
@@ -129,6 +130,7 @@ public class ViewAlignment {
 
 			final RealTransform[] realTransforms = new RealTransform[datasetNames.length];
 			for (int i = 0; i < datasetNames.length; ++i) {
+				System.out.println( "Loading: " +  group + "/" + transformDatasetNames[i] );
 				realTransforms[i] = Transform.loadScaledTransform(
 						n5,
 						group + "/" + transformDatasetNames[i]);
