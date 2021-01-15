9774a54345c7adb3a089224afb0bc0dfc23f60a6
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-01-15 06:17:28 -0500

    computing overlap intervals

--- src/main/java/org/janelia/saalfeldlab/ispim/SparkPairwiseStitchSlabs.java
+++ src/main/java/org/janelia/saalfeldlab/ispim/SparkPairwiseStitchSlabs.java
@@ -4,11 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+
+import org.janelia.saalfeldlab.ispim.SparkPaiwiseAlignChannelsGeo.N5Data;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -17,6 +23,15 @@ import com.google.gson.stream.JsonReader;
 import loci.formats.FormatException;
 import mpicbg.models.IllDefinedDataPointsException;
 import mpicbg.models.NotEnoughDataPointsException;
+import net.imglib2.Dimensions;
+import net.imglib2.FinalInterval;
+import net.imglib2.FinalRealInterval;
+import net.imglib2.Interval;
+import net.imglib2.RealInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Util;
+import net.preibisch.mvrecon.fiji.spimdata.interestpoints.InterestPoint;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
@@ -57,14 +72,137 @@ public class SparkPairwiseStitchSlabs implements Callable<Void>, Serializable {
 			final String n5Path,
 			final String idA,
 			final String idB,
+			final MetaData metaA,
+			final MetaData metaB,
 			final String channelA,
 			final String channelB,
 			final String camA,
 			final String camB,
-			final int[] blockSize,
+			final int blockSize,
 			final boolean doICP ) throws IOException, FormatException, NotEnoughDataPointsException, IllDefinedDataPointsException
 	{
-		
+		System.out.println( new Date(System.currentTimeMillis() ) + ": Opening N5." );
+
+		final N5Data n5dataA = SparkPaiwiseAlignChannelsGeo.openN5( n5Path, idA );
+		final N5Data n5dataB = SparkPaiwiseAlignChannelsGeo.openN5( n5Path, idB );
+
+		System.out.println( new Date(System.currentTimeMillis() ) + ": lastSliceIndexA=" + n5dataA.lastSliceIndex + " for " + idA );
+		System.out.println( new Date(System.currentTimeMillis() ) + ": lastSliceIndexB=" + n5dataB.lastSliceIndex + " for " + idB );
+
+		ArrayList<InterestPoint> pointsChA = null;
+		ArrayList<InterestPoint> pointsChB = null;
+
+		System.out.println( "loading points ... " );
+
+		try
+		{
+			final String datasetNameA = idA + "/" + channelA + "/Stack-DoG-detections";
+			final DatasetAttributes datasetAttributesA = n5dataA.n5.getDatasetAttributes(datasetNameA);
+
+			final String datasetNameB = idB + "/" + channelB + "/Stack-DoG-detections";
+			final DatasetAttributes datasetAttributesB = n5dataB.n5.getDatasetAttributes(datasetNameB);
+
+			pointsChA = n5dataA.n5.readSerializedBlock(datasetNameA, datasetAttributesA, new long[] {0});
+			pointsChB = n5dataB.n5.readSerializedBlock(datasetNameB, datasetAttributesB, new long[] {0});
+		}
+		catch ( Exception e ) // java.nio.file.NoSuchFileException
+		{
+			e.printStackTrace();
+			System.out.println( new Date(System.currentTimeMillis() ) + ": Failed to load points for " + idA + " <> " + idB );
+			return;
+		}
+
+		System.out.println( new Date(System.currentTimeMillis() ) + ": channelA: " + pointsChA.size() + " points for " + idA );
+		System.out.println( new Date(System.currentTimeMillis() ) + ": channelB: " + pointsChB.size() + " points for " + idB );
+
+		RealInterval bbA = realInterval( pointsChA );
+		RealInterval bbB = realInterval( pointsChB );
+
+		System.out.println( Intervals.toString( bbA ) );
+		System.out.println( Intervals.toString( bbB ) );
+
+		System.out.println( "transforming points to stage coordinates ... " );
+
+		List< InterestPoint > pointsChANew = new ArrayList<InterestPoint>();
+		for ( final InterestPoint p : pointsChA )
+		{
+			final double[] l = p.getL().clone();
+			l[ 0 ] += metaA.position[ 0 ];
+			l[ 1 ] += metaA.position[ 1 ];
+			l[ 2 ] += metaA.position[ 2 ];
+			pointsChANew.add( new InterestPoint( p.getId(), l ) );
+		}
+
+		List< InterestPoint > pointsChBNew = new ArrayList<InterestPoint>();
+		for ( final InterestPoint p : pointsChB )
+		{
+			final double[] l = p.getL().clone();
+			l[ 0 ] += metaB.position[ 0 ];
+			l[ 1 ] += metaB.position[ 1 ];
+			l[ 2 ] += metaB.position[ 2 ];
+			pointsChBNew.add( new InterestPoint( p.getId(), l ) );
+		}
+
+		bbA = realInterval( pointsChANew );
+		bbB = realInterval( pointsChBNew );
+
+		System.out.println( Intervals.toString( bbA ) );
+		System.out.println( Intervals.toString( bbB ) );
+
+		RealInterval overlap = Intervals.intersect( bbA, bbB );
+		RealInterval expanded = expand( overlap, new double[] { 50, 50, 50 } );
+
+		System.out.println( "overlap: " + Intervals.toString( overlap ) );
+		System.out.println( "expanded: " + Intervals.toString( expanded ) + ", size=" + Util.printCoordinates( size( expanded ) ) );
+	}
+
+	public static double[] size( final RealInterval interval )
+	{
+		final double[] size = new double[ interval.numDimensions() ];
+		for ( int d = 0; d < interval.numDimensions(); ++d )
+			size[ d ] = interval.realMax( d ) - interval.realMin( d );
+		return size;
+	}
+
+	public static FinalRealInterval expand( final RealInterval interval, final double[] border )
+	{
+		final int n = interval.numDimensions();
+		final double[] min = new double[ n ];
+		final double[] max = new double[ n ];
+		interval.realMin(min);
+		interval.realMax(max);
+		for ( int d = 0; d < n; ++d )
+		{
+			min[ d ] -= border[ d ];
+			max[ d ] += border[ d ];
+		}
+		return new FinalRealInterval( min, max );
+	}
+
+	public static RealInterval realInterval( Collection< ? extends RealLocalizable > points )
+	{
+		final int n = points.iterator().next().numDimensions();
+
+		double[] min = new double[ n ];
+		double[] max = new double[ n ];
+
+		for ( int d = 0; d < n; ++d )
+		{
+			min[ d ] = Double.MAX_VALUE;
+			max[ d ] = -Double.MAX_VALUE;
+		}
+
+		for ( final RealLocalizable l : points )
+		{
+			for ( int d = 0; d < n; ++d )
+			{
+				final double p = l.getDoublePosition( d );
+				min[ d ] = Math.min( min[ d ], p );
+				max[ d ] = Math.max( max[ d ], p );
+			}
+		}
+
+		return new FinalRealInterval( min, max );
 	}
 
 	public static HashMap< String, MetaData > readPositionMetaData( final String positionFile ) throws FileNotFoundException
@@ -97,11 +235,13 @@ public class SparkPairwiseStitchSlabs implements Callable<Void>, Serializable {
 	}
 
 	@Override
-	public Void call() throws IOException
+	public Void call() throws IOException, FormatException, NotEnoughDataPointsException, IllDefinedDataPointsException
 	{
 		System.out.println( idA + " <> " + idB );
 
 		final HashMap< String, MetaData > meta = readPositionMetaData( positionFile );
+
+		align( n5Path, idA, idB, meta.get( idA ), meta.get( idB ), channelA, channelB, camA, camB, 50, true );
 
 		return null;
 	}
