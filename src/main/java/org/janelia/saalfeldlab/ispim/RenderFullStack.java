74e88635391ed8aae5a1ac871646ba02f250905a
Stephan Preibisch <preibischs@janelia.hhmi.org>
2021-01-21 11:18:42 -0500

    added BDV flythrough

--- src/main/java/org/janelia/saalfeldlab/ispim/RenderFullStack.java
+++ src/main/java/org/janelia/saalfeldlab/ispim/RenderFullStack.java
@@ -1,17 +1,28 @@
 package org.janelia.saalfeldlab.ispim;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+
 import org.janelia.saalfeldlab.hotknife.util.Lazy;
 import org.janelia.saalfeldlab.ispim.SparkPaiwiseAlignChannelsGeo.N5Data;
+import org.janelia.saalfeldlab.ispim.bdv.BDVFlyThrough;
 import org.janelia.saalfeldlab.ispim.imglib2.VirtualRasterDataLoader;
 import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Writer;
+import org.scijava.ui.behaviour.KeyStrokeAdder;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.io.InputTriggerDescription;
+import org.scijava.ui.behaviour.util.AbstractNamedAction;
 
 import com.google.gson.GsonBuilder;
 
@@ -38,6 +49,7 @@ import net.imglib2.util.Pair;
 import net.imglib2.util.Util;
 import net.imglib2.util.ValuePair;
 import net.imglib2.view.Views;
+import net.preibisch.mvrecon.fiji.spimdata.explorer.util.ColorStream;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
@@ -82,8 +94,8 @@ public class RenderFullStack implements Callable<Void>, Serializable
 
 		final Interval interval = new FinalInterval( min, max );
 
-		System.out.println( id + "," + channel + "," + cam + ": interval=" + interval );
-		System.out.println( id + "," + channel + "," + cam + ": affine3d=" + n5data.affine3D.get( channel ) );
+		System.out.println( new Date(System.currentTimeMillis() ) + ": " + id + "," + channel + "," + cam + ": interval=" + interval );
+		System.out.println( new Date(System.currentTimeMillis() ) + ": " + id + "," + channel + "," + cam + ": affine3d=" + n5data.affine3D.get( channel ) );
 
 		final VirtualRasterDataLoader< UnsignedShortType > loader =
 				new VirtualRasterDataLoader<UnsignedShortType>(
@@ -97,8 +109,8 @@ public class RenderFullStack implements Callable<Void>, Serializable
 						new UnsignedShortType() );
 
 		final int blockSizeXY = (int)Math.min( interval.dimension( 0 ), interval.dimension( 1 ) );
-		final int[] blockSize = new int[] { blockSizeXY, blockSizeXY, 40 };
-		System.out.println( id + "," + channel + "," + cam + ": blocksize=" + Util.printCoordinates( blockSize ) );
+		final int[] blockSize = new int[] { blockSizeXY, blockSizeXY, 10 };
+		System.out.println( new Date(System.currentTimeMillis() ) + ": " + id + "," + channel + "," + cam + ": blocksize=" + Util.printCoordinates( blockSize ) );
 
 		final RandomAccessibleInterval<UnsignedShortType> cachedImg =
 				Views.translate(
@@ -133,7 +145,105 @@ public class RenderFullStack implements Callable<Void>, Serializable
 		bdv = SparkPaiwiseAlignChannelsGeo.displayCam( bdv, channel, cam, n5data.stacks.get( channel ).get( cam ), n5data.alignments.get( channel ), n5data.camTransforms.get( channel ).get( cam ), new AffineTransform3D(), 0, n5data.lastSliceIndex );
 		bdv.setDisplayRange( 0, 1000 );
 		bdv.setColor( new ARGBType( ARGBType.rgba(255, 0, 255, 0)));
-		
+	}
+
+	protected static void testTwoStacks(
+			final String n5Path,
+			final String idA, final String channelA, final String camA,
+			final String idB, final String channelB, final String camB ) throws IOException, FormatException
+	{
+		final int numFetchThreads = Runtime.getRuntime().availableProcessors() / 2;
+		System.out.println("building SharedQueue with " + numFetchThreads + " FetcherThreads" );
+		final SharedQueue queue = new SharedQueue(numFetchThreads, 1 );
+
+		Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > stackA = loadStack( n5Path, idA, channelA, camA, queue );
+		Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > stackB = loadStack( n5Path, idB, channelB, camB, queue );
+
+		System.out.println( "Loaded all stacks." );
+
+		final Scale3D anisotropy = new Scale3D(0.2, 0.2, 0.85);
+
+		BdvOptions options;
+		BdvStackSource<?> bdv;
+
+		options = BdvOptions.options().sourceTransform( stackA.getB().affine3D.get( channelA ).preConcatenate( anisotropy ) ).numRenderingThreads( numFetchThreads );
+		bdv = BdvFunctions.show( Views.extendValue( stackA.getA(), 0 ), new FinalInterval( stackA.getA() ), idA + "," + channelA + "," + camA, options );
+		bdv.setDisplayRange( 0, 1000 );
+		bdv.setColor( new ARGBType( ARGBType.rgba(0, 255, 0, 0)));
+
+		options = BdvOptions.options().sourceTransform( stackB.getB().affine3D.get( channelB ).preConcatenate( anisotropy ) ).numRenderingThreads( numFetchThreads ).addTo( bdv );
+		bdv = BdvFunctions.show( Views.extendValue( stackB.getA(), 0 ), new FinalInterval( stackB.getA() ), idB + "," + channelB + "," + camB, options );
+		bdv.setDisplayRange( 0, 1000 );
+		bdv.setColor( new ARGBType( ARGBType.rgba(255, 0, 255, 0)));
+
+		setupRecordMovie( bdv );
+	}
+
+	public static void setupRecordMovie( final BdvStackSource<?> bdvSource )
+	{
+		final ActionMap ksActionMap = new ActionMap();
+		final InputMap ksInputMap = new InputMap();
+
+		// default input trigger config, disables "control button1" drag in bdv
+		// (collides with default of "move annotation")
+		final InputTriggerConfig config = new InputTriggerConfig(
+				Arrays.asList(
+						new InputTriggerDescription[]{
+								new InputTriggerDescription(
+										new String[]{"not mapped"}, "drag rotate slow", "bdv")}));
+
+		final KeyStrokeAdder ksKeyStrokeAdder = config.keyStrokeAdder(ksInputMap, "persistence");
+
+		new AbstractNamedAction( "Record movie" )
+		{
+			private static final long serialVersionUID = 3640052275162419689L;
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				new Thread( ()-> BDVFlyThrough.record( bdvSource.getBdvHandle().getViewerPanel() ) ).start();
+			}
+
+			public void register() {
+				put(ksActionMap);
+				ksKeyStrokeAdder.put(name(), "ctrl R" );
+			}
+		}.register();
+
+		new AbstractNamedAction( "Add Current Viewer Transform" )
+		{
+			private static final long serialVersionUID = 3620052275162419689L;
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				BDVFlyThrough.addCurrentViewerTransform( bdvSource.getBdvHandle().getViewerPanel() );
+			}
+
+			public void register() {
+				put(ksActionMap);
+				ksKeyStrokeAdder.put(name(), "ctrl A" );
+			}
+		}.register();
+
+		new AbstractNamedAction( "Clear All Viewer Transforms" )
+		{
+			private static final long serialVersionUID = 3620052275162419689L;
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				BDVFlyThrough.clearAllViewerTransform();
+			}
+
+			public void register() {
+				put(ksActionMap);
+				ksKeyStrokeAdder.put(name(), "ctrl X" );
+			}
+		}.register();
+
+		bdvSource.getBdvHandle().getKeybindings().addActionMap("persistence", ksActionMap);
+		bdvSource.getBdvHandle().getKeybindings().addInputMap("persistence", ksInputMap);
 	}
 
 	@Override
@@ -153,31 +263,37 @@ public class RenderFullStack implements Callable<Void>, Serializable
 		Collections.sort( allIds );
 
 		//testRendering( n5Path, allIds.get( 0 ), channel, cam );
+		testTwoStacks( n5Path, allIds.get( 0 ), channel, cam, allIds.get( 1 ), channel, cam );
+
+		SimpleMultiThreading.threadHaltUnClean();
 
 		final int numFetchThreads = Runtime.getRuntime().availableProcessors() / 2;
-		System.out.println("building SharedQueue with " + numFetchThreads + " FetcherThreads" );
+		System.out.println( new Date(System.currentTimeMillis() ) + ": building SharedQueue with " + numFetchThreads + " FetcherThreads" );
 		final SharedQueue queue = new SharedQueue(numFetchThreads, 1 );
 
-		Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > stack0 = loadStack( n5Path, allIds.get( 0 ), channel, cam, queue );
-		Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > stack1 = loadStack( n5Path, allIds.get( 1 ), channel, cam, queue );
+		final ArrayList< Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > > stacks = new ArrayList<>();
 
-		System.out.println( "Loaded all stacks." );
+		for ( final String id : allIds )
+			stacks.add( loadStack( n5Path, id, channel, cam, queue ) );
 
-		//final AffineTransform3D anisotropy = new AffineTransform3D();
+		System.out.println( new Date(System.currentTimeMillis() ) + ": Loaded all stacks." );
+
 		final Scale3D anisotropy = new Scale3D(0.2, 0.2, 0.85);
 
-		BdvOptions options;
-		BdvStackSource<?> bdv;
+		BdvStackSource<?> bdv = null;
 
-		options = BdvOptions.options().sourceTransform( stack0.getB().affine3D.get( channel ).preConcatenate( anisotropy ) ).numRenderingThreads( numFetchThreads );
-		bdv = BdvFunctions.show( Views.extendValue( stack0.getA(), 0 ), new FinalInterval( stack0.getA() ), allIds.get( 0 ) + "," + channel + "," + cam, options );
-		bdv.setDisplayRange( 0, 1000 );
-		bdv.setColor( new ARGBType( ARGBType.rgba(0, 255, 0, 0)));
+		for ( final Pair< RandomAccessibleInterval< VolatileUnsignedShortType >, N5Data > stack : stacks )
+		{
+			BdvOptions options = BdvOptions.options().sourceTransform( stack.getB().affine3D.get( channel ).preConcatenate( anisotropy ) ).numRenderingThreads( numFetchThreads ).addTo( bdv );
 
-		options = BdvOptions.options().sourceTransform( stack1.getB().affine3D.get( channel ).preConcatenate( anisotropy ) ).numRenderingThreads( numFetchThreads ).addTo( bdv );
-		bdv = BdvFunctions.show( Views.extendValue( stack1.getA(), 0 ), new FinalInterval( stack1.getA() ), allIds.get( 1 ) + "," + channel + "," + cam, options );
-		bdv.setDisplayRange( 0, 1000 );
-		bdv.setColor( new ARGBType( ARGBType.rgba(255, 0, 255, 0)));
+			bdv = BdvFunctions.show( Views.extendValue( stack.getA(), 0 ), new FinalInterval( stack.getA() ), stack.getB().id + "," + channel + "," + cam, options );
+			bdv.setDisplayRange( 0, 1000 );
+			bdv.setColor( new ARGBType( ColorStream.next() ) );
+		}
+
+		System.out.println( new Date(System.currentTimeMillis() ) + ": Displayed all stacks." );
+
+		setupRecordMovie( bdv );
 
 		SimpleMultiThreading.threadHaltUnClean();
 		return null;
