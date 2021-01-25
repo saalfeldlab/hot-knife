3ecdaf9bc2b4aaf0a9475b7683d4ee9b07ac9d98
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-01-25 10:22:42 -0500

    making small vis for presentation

---
+++ src/main/java/org/janelia/saalfeldlab/ispim/PresentationTools.java
@@ -0,0 +1,47 @@
+package org.janelia.saalfeldlab.ispim;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.janelia.saalfeldlab.ispim.SparkPaiwiseAlignChannelsGeo.N5Data;
+
+import ij.ImageJ;
+import loci.formats.FormatException;
+import loci.formats.in.TiffReader;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
+import net.imglib2.realtransform.RealViews;
+import net.imglib2.view.Views;
+
+public class PresentationTools {
+
+	public static void loadStackRaw( final String n5Path, final String id, final String channel, final String cam ) throws IOException, FormatException
+	{
+		N5Data n5 = SparkPaiwiseAlignChannelsGeo.openN5( n5Path, id );
+
+		final int width, height;
+		{
+			try(final TiffReader firstSliceReader = new TiffReader()) {
+
+				firstSliceReader.setId( n5.stacks.get( channel ).get( cam ).get(0).path);
+				width = firstSliceReader.getSizeX();
+				height = firstSliceReader.getSizeY();
+				firstSliceReader.close();
+			}
+		}
+
+		ArrayList<Integer> slices = new ArrayList<>();
+		for ( int i = 0; i <= n5.lastSliceIndex; ++i )
+			slices.add( i );
+
+		SparkExtractGeometricPointDescriptorMatches.showStack(n5.stacks.get( channel ).get( cam ), slices, width, height);
+	}
+
+	public static void main( String[] args ) throws IOException, FormatException
+	{
+		final String n5Path = "/nrs/saalfeld/from_mdas/mar24_bis25_s5_r6-backup.n5";
+
+		new ImageJ();
+		loadStackRaw( n5Path, "Pos001", "Ch488+561+647nm", "cam1");
+	}
+}
