b679407ce9b9258a87575713dd85fe04116ea062
tpietzsch <tobias.pietzsch@gmail.com>
2021-10-08 23:35:44 +0200

    extract GaussTransform to separate class

---
+++ src/main/java/org/janelia/saalfeldlab/hotknife/tobi/GaussTransform.java
@@ -0,0 +1,94 @@
+package org.janelia.saalfeldlab.hotknife.tobi;
+
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.RealPositionable;
+import net.imglib2.realtransform.RealTransform;
+
+public class GaussTransform implements RealTransform {
+
+//	private double maxSlope;
+	private double minSigma;
+	private double exph;
+
+	public GaussTransform() {
+		this(0.8, 100.0);
+	}
+
+	public GaussTransform(double maxSlope, double minSigma) {
+//		this.maxSlope = maxSlope;
+		this.minSigma = minSigma;
+		this.exph = Math.exp(-0.5) / maxSlope;
+	}
+
+	public void setMinSigma(double minSigma) {
+		this.minSigma = minSigma;
+	}
+
+	public void setMaxSlope(double maxSlope) {
+//		this.maxSlope = maxSlope;
+		this.exph = Math.exp(-0.5) / maxSlope;
+	}
+
+	private boolean active = false;
+//	private double sx0;
+//	private double sy0;
+	private double sx1;
+	private double sy1;
+	private double stx; // sx1 - sx0
+	private double sty; // sy1 - sy0
+
+	public void setLine(double sx0, double sy0, double sx1, double sy1) {
+//		this.sx0 = sx0;
+//		this.sy0 = sy0;
+		this.sx1 = sx1;
+		this.sy1 = sy1;
+		stx = sx0 - sx1;
+		sty = sy0 - sy1;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public int numSourceDimensions() {
+		return 2;
+	}
+
+	@Override
+	public int numTargetDimensions() {
+		return 2;
+	}
+
+	@Override
+	public void apply(final double[] source, final double[] target) {
+		apply(RealPoint.wrap(source), RealPoint.wrap(target));
+	}
+
+	@Override
+	public void apply(final RealLocalizable source, final RealPositionable target) {
+		if (active) {
+			double x = source.getDoublePosition(0);
+			double y = source.getDoublePosition(1);
+
+			double h = Math.sqrt(stx * stx + sty * sty);
+			double sigma = Math.max(minSigma, h * exph);
+			double asqu = (x - sx1) * (x - sx1) + (y - sy1) * (y - sy1);
+			double dt = Math.exp(asqu / (-2.0 * sigma * sigma));
+
+			double transformedX = x + dt * stx;
+			double transformedY = y + dt * sty;
+
+			target.setPosition(transformedX, 0);
+			target.setPosition(transformedY, 1);
+		} else {
+			target.setPosition(source);
+		}
+	}
+
+	@Override
+	public RealTransform copy() {
+		return this;
+	}
+}
--- src/main/java/org/janelia/saalfeldlab/hotknife/tobi/ViewAlignmentPlayground4.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/tobi/ViewAlignmentPlayground4.java
@@ -10,12 +10,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.io.IOException;
-import net.imglib2.RealLocalizable;
-import net.imglib2.RealPoint;
-import net.imglib2.RealPositionable;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.realtransform.AffineTransform3D;
-import net.imglib2.realtransform.RealTransform;
 import org.janelia.saalfeldlab.n5.N5FSReader;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.scijava.ui.behaviour.DragBehaviour;
@@ -40,20 +36,22 @@ public class ViewAlignmentPlayground4 {
 
 		final PositionField positionField = new PositionField(n5, transformGroup);
 
-		final TransformEditor editor = new TransformEditor();
-		final TransformedSurfacePyramid<?, ?> tpyramid = new TransformedSurfacePyramid<>(pyramid, positionField, editor.getTransform());
+		final GaussTransform movingTransform = new GaussTransform();
+		final TransformedSurfacePyramid<?, ?> tpyramid = new TransformedSurfacePyramid<>(pyramid, positionField, movingTransform);
 		final BdvStackSource<?> source = BdvFunctions.show(tpyramid.getSourceAndConverter(), Bdv.options().is2D());
 		source.setDisplayRange(0, 255);
 		source.setDisplayRangeBounds(0, 255);
-		editor.install(source);
+
+		final GaussTransformEditor editor = new GaussTransformEditor(movingTransform, source);
 	}
 
-	static class TransformEditor {
+	static class GaussTransformEditor {
 
-		private ViewerPanel viewer;
+		private final GaussTransform transform;
+		private final ViewerPanel viewer;
 
-		public void install(Bdv bdv) {
-
+		public GaussTransformEditor(final GaussTransform movingTransform, final Bdv bdv) {
+			transform = movingTransform;
 			viewer = bdv.getBdvHandle().getViewerPanel();
 
 			final Behaviours behaviours = new Behaviours(new InputTriggerConfig());
@@ -64,67 +62,6 @@ public class ViewAlignmentPlayground4 {
 			viewer.getDisplay().overlays().add(overlay);
 		}
 
-		public RealTransform getTransform() {
-			return new RealTransform() {
-
-				@Override
-				public int numSourceDimensions() {
-					return 2;
-				}
-
-				@Override
-				public int numTargetDimensions() {
-					return 2;
-				}
-
-				@Override
-				public void apply(final double[] source, final double[] target) {
-					apply(RealPoint.wrap(source), RealPoint.wrap(target));
-				}
-
-				@Override
-				public void apply(final RealLocalizable source, final RealPositionable target) {
-					transform(source, target);
-				}
-
-				@Override
-				public RealTransform copy() {
-					return this;
-				}
-			};
-		}
-
-		private static final double maxSlope = 0.8;
-		private static final double exph = Math.exp(-0.5) / maxSlope;
-		private static final double minSigma = 100.0;
-
-		private void transform(final RealLocalizable source, final RealPositionable target) {
-			if (dragging) {
-				double x = source.getDoublePosition(0);
-				double y = source.getDoublePosition(1);
-
-				double h = Math.sqrt(stx * stx + sty * sty);
-				double sigma = Math.max(minSigma, h * exph);
-				double asqu = (x - sx1) * (x - sx1) + (y - sy1) * (y - sy1);
-				double dt = Math.exp(asqu / (-2.0 * sigma * sigma));
-
-				double transformedX = x + dt * stx;
-				double transformedY = y + dt * sty;
-
-				target.setPosition(transformedX, 0);
-				target.setPosition(transformedY, 1);
-			} else {
-				target.setPosition(source);
-			}
-		}
-
-		private double stx;
-		private double sty;
-		private double sx0;
-		private double sy0;
-		private double sx1;
-		private double sy1;
-
 		private boolean dragging = false;
 		private int x0;
 		private int y0;
@@ -132,11 +69,8 @@ public class ViewAlignmentPlayground4 {
 		private int y1;
 
 		private synchronized void initTranslate(final int x, final int y) {
-			dragging = true;
 			x0 = x;
 			y0 = y;
-			x1 = x;
-			y1 = y;
 		}
 
 		private synchronized void dragTranslate(final int x, final int y) {
@@ -151,27 +85,23 @@ public class ViewAlignmentPlayground4 {
 					tmp.get(1, 0), tmp.get(1, 1), tmp.get(1, 3));
 			double[] source = new double[2];
 			sourceToViewer.applyInverse(source, new double[]{x0, y0});
-			sx0 = source[0];
-			sy0 = source[1];
+			final double sx0 = source[0];
+			final double sy0 = source[1];
 			sourceToViewer.applyInverse(source, new double[]{x1, y1});
-			sx1 = source[0];
-			sy1 = source[1];
-			stx = sx0 - sx1;
-			sty = sy0 - sy1;
+			final double sx1 = source[0];
+			final double sy1 = source[1];
 
+			transform.setLine(sx0, sy0, sx1, sy1);
+			transform.setActive(true);
+
+			dragging = true;
 			viewer.requestRepaint();
 		}
 
 		private synchronized void endTranslate(final int x, final int y) {
+			transform.setActive(false);
+
 			dragging = false;
-			x1 = x;
-			y1 = y;
-
-			sx0 = sx1;
-			sy0 = sy1;
-			stx = 0;
-			sty = 0;
-
 			viewer.requestRepaint();
 		}
 
@@ -209,9 +139,7 @@ public class ViewAlignmentPlayground4 {
 
 			@Override
 			public void setCanvasSize(final int width, final int height) {
-
 			}
 		}
 	}
-
 }
