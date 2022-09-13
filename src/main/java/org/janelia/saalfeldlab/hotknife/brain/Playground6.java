40dc2038a9c216ca6d450f7c4f2669855682c63d
tpietzsch <tobias.pietzsch@gmail.com>
2022-09-13 15:20:26 +0200

    WIP

---
+++ src/main/java/org/janelia/saalfeldlab/hotknife/brain/Playground6.java
@@ -0,0 +1,194 @@
+package org.janelia.saalfeldlab.hotknife.brain;
+
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.util.BdvSource;
+import bdv.util.volatiles.VolatileViews;
+import bdv.viewer.ViewerPanel;
+import java.io.IOException;
+import java.util.Arrays;
+import net.imglib2.RandomAccessible;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.RealPositionable;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.interpolation.randomaccess.ClampingNLinearInterpolatorFactory;
+import net.imglib2.realtransform.InverseRealTransform;
+import net.imglib2.realtransform.RealTransform;
+import net.imglib2.realtransform.RealTransformRealRandomAccessible;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.ConstantUtils;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+import org.janelia.saalfeldlab.hotknife.FlattenTransform;
+import org.janelia.saalfeldlab.hotknife.brain.Playground3.MyHeightField;
+import org.janelia.saalfeldlab.hotknife.util.Transform;
+import org.janelia.saalfeldlab.n5.N5FSReader;
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+
+import static org.janelia.saalfeldlab.hotknife.brain.Playground5.extendHeightfield;
+import static org.janelia.saalfeldlab.hotknife.brain.Playground5.lscale;
+
+public class Playground6 {
+
+	public static void main(String[] args) throws IOException {
+
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+
+		// --------------------------------------------------------------------
+		// load and crop image
+		// (the crop region covers the full image in Y and Z)
+		// --------------------------------------------------------------------
+		final String n5Path = "/Users/pietzsch/Desktop/data/janelia/Z0720_07m_VNC/s5/";
+		final String imgGroup = ".";
+
+		final N5Reader n5 = new N5FSReader(n5Path);
+		final RandomAccessibleInterval<UnsignedByteType> imgBrain = N5Utils.openVolatile(n5, imgGroup);
+		final int n5Level = 5;
+
+		final long[] minIntervalS0 = {47204, 46557, 42756};
+		final long[] maxIntervalS0 = {55779, 59038, 53664};
+		final long[] minInterval = lscale(minIntervalS0, n5Level);
+		final long[] maxInterval = lscale(maxIntervalS0, n5Level);
+		final long[] translation = {-minInterval[1], -minInterval[2], maxInterval[0]};
+
+		minInterval[1] = imgBrain.min(1);
+		minInterval[2] = imgBrain.min(2);
+		maxInterval[1] = imgBrain.max(1);
+		maxInterval[2] = imgBrain.max(2);
+
+		final IntervalView<UnsignedByteType> crop1 = Views.interval(imgBrain, minInterval, maxInterval);
+		final RandomAccessibleInterval<UnsignedByteType> crop2 = Views.rotate(crop1, 1, 0 );
+		final RandomAccessibleInterval<UnsignedByteType> crop3 = Views.permute(crop2, 1, 2 );
+		final IntervalView<UnsignedByteType> crop = Views.translate(crop3, translation);
+
+
+
+		// --------------------------------------------------------------------
+		// load and expand heightfield
+		// --------------------------------------------------------------------
+		final MyHeightField hf = new MyHeightField("/Users/pietzsch/Desktop/data/janelia/Z0720_07m_VNC/heightfield/", ".", new double[] {6, 6, 1}, 4658.6666161072235);
+		final RandomAccessibleInterval<FloatType> heightfield = hf.heightfield();
+		final double avg = hf.avg();
+		final double[] plane = {2.004294094052206, -1.8362464688517335, 4243.432822291761};
+		final int fadeToPlaneDist = 1000;
+		final int fadeToAvgDist = 2000;
+		final RandomAccessible<FloatType> extendHeightfield = extendHeightfield(heightfield, avg, plane, fadeToPlaneDist, fadeToAvgDist);
+
+
+
+		// --------------------------------------------------------------------
+		// flatten crop with expanded heightfield
+		// --------------------------------------------------------------------
+		final double[] hfDownsamplingFactors = hf.downsamplingFactors();
+		final double[] n5DownsamplingFactors = {1 << n5Level, 1 << n5Level, 1 << n5Level};
+		final double[] hfRelativeScale = new double[3];
+		Arrays.setAll(hfRelativeScale, d -> hfDownsamplingFactors[d] / n5DownsamplingFactors[d]);
+		final RealRandomAccessible<DoubleType> scaledHeightfield = Transform.scaleAndShiftHeightFieldAndValues(extendHeightfield, hfRelativeScale);
+
+		final double scaledAvg = (avg + 0.5) * hfRelativeScale[2] - 0.5;
+
+		final long max = crop.max(2);
+		final RealRandomAccessible<DoubleType> constHeightfield = ConstantUtils.constantRealRandomAccessible(new DoubleType(max), 2);
+		final FlattenTransform<DoubleType> flattenTransform = new FlattenTransform<>(
+				scaledHeightfield,
+				constHeightfield,
+				scaledAvg, max);
+
+//		final RealTransform flatten = flattenTransform.inverse();
+		final RealTransform flatten = new ExtendedFlattenTransform(
+				flattenTransform.inverse(),
+				scaledAvg, max, 1000);
+		final RealRandomAccessible<UnsignedByteType> flattenedCrop = new RealTransformRealRandomAccessible<>(
+				Views.interpolate(
+						Views.extendValue(crop, new UnsignedByteType()),
+						new ClampingNLinearInterpolatorFactory<>()),
+				flatten);
+
+
+
+		// --------------------------------------------------------------------
+		// show in BDV: crop, flattened crop, extended heightfield
+		// --------------------------------------------------------------------
+		final BdvSource bdv = BdvFunctions.show(VolatileViews.wrapAsVolatile(crop), "crop", Bdv.options());
+		final BdvSource flattenedCropSource = BdvFunctions.show(flattenedCrop, crop, "flattened", Bdv.options().addTo(bdv));
+
+
+
+		final RandomAccessible<DoubleType> hfExpanded = Views.addDimension(Views.raster(scaledHeightfield));//, crop.min(2), crop.max(2));
+		final BdvSource hfSource = BdvFunctions.show(Views.interval(hfExpanded, crop), "heightfield", Bdv.options().addTo(bdv));
+		hfSource.setDisplayRangeBounds(0, 255);
+		hfSource.setDisplayRange(0, 255);
+		hfSource.setColor(new ARGBType(0x00ff00));
+
+		final ViewerPanel viewerPanel = bdv.getBdvHandle().getViewerPanel();
+		final CoordinatesAndValuesOverlay overlay = new CoordinatesAndValuesOverlay(viewerPanel);
+		viewerPanel.getDisplay().overlays().add(overlay);
+	}
+
+	static class ExtendedFlattenTransform implements RealTransform {
+
+		private final RealTransform invFlatten;
+		private final double minZ;
+		private final double maxZ;
+		private double fadeDist;
+
+		final RealPoint flattened = new RealPoint(3);
+
+		public ExtendedFlattenTransform(final RealTransform invFlatten, final double minZ, final double maxZ, final double fadeDist) {
+			assert invFlatten.numSourceDimensions() == 3;
+			assert invFlatten.numTargetDimensions() == 3;
+			this.invFlatten = invFlatten;
+			this.minZ = minZ;
+			this.maxZ = maxZ;
+			this.fadeDist = fadeDist;
+		}
+
+		@Override
+		public int numSourceDimensions() {
+			return 3;
+		}
+
+		@Override
+		public int numTargetDimensions() {
+			return 3;
+		}
+
+		@Override
+		public void apply(final double[] source, final double[] target) {
+			apply(RealPoint.wrap(source), RealPoint.wrap(target));
+		}
+
+		@Override
+		public void apply(final RealLocalizable source, final RealPositionable target) {
+			final double z = source.getDoublePosition(2);
+			if (z > maxZ)
+				target.setPosition(source);
+			else if (z > minZ)
+				invFlatten.apply(source, target);
+			else {
+				final double dd = ( minZ - z ) * 4.0 / fadeDist;
+				final double s = Math.exp(dd * dd / (-2.0));
+				invFlatten.apply(source, flattened);
+				final double x = source.getDoublePosition(0);
+				final double y = source.getDoublePosition(1);
+				final double fx = flattened.getDoublePosition(0);
+				final double fy = flattened.getDoublePosition(1);
+				final double fz = flattened.getDoublePosition(2);
+				target.setPosition(s * (fx - x) + x, 0);
+				target.setPosition(s * (fy - y) + y, 1);
+				target.setPosition(s * (fz - z) + z, 2);
+			}
+		}
+
+		@Override
+		public RealTransform copy() {
+			return new ExtendedFlattenTransform(invFlatten.copy(), minZ, maxZ, fadeDist);
+		}
+	}
+}
