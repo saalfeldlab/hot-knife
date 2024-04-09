397dcc8cac5e97ac3529cced1afdc89b42ec9010
Eric Trautman <trautmane@janelia.hhmi.org>
2024-04-09 16:53:32 -0400

    update multi-SEM 91 export script wafer-53d data

--- run_scripts/multi-sem/wafer-53/91_export_aligned_slab_series_batch.sh
+++ run_scripts/multi-sem/wafer-53/91_export_aligned_slab_series_batch.sh
@@ -29,19 +29,22 @@ export RUNTIME=${3:-4320} # default is minutes in 3 days
 # --------------------------------------------------------------------
 # setup export parameters
 
-N5_PATH="/nrs/hess/render/export/hess.n5"
-TRANSFORM_GROUP="/surface-align/run_20230404_105038/pass12"
-DATA_SET_OUTPUT="/wafer-52-align/run_20230404_105038/pass12/s0"
+N5_PATH="/nrs/hess/data/hess_wafer_53/export/hess_wafer_53d.n5"
+TRANSFORM_GROUP="/surface-align/run_20240409_135204/pass12"
+DATA_SET_OUTPUT="/wafer-53-align/run_20240409_135204/pass12/s0"
 
 ARGV="\
 --n5PathInput ${N5_PATH} \
--i /flat/cut_030_slab_026/raw -t 20 -b -21 \
--i /flat/cut_031_slab_006/raw -t 20 -b -21 \
--i /flat/cut_032_slab_013/raw -t 20 -b -21 \
--i /flat/cut_033_slab_033/raw -t 20 -b -21 \
--i /flat/cut_034_slab_020/raw -t 20 -b -21 \
--i /flat/cut_035_slab_001/raw -t 20 -b -21 \
--i /flat/cut_036_slab_045/raw -t 20 -b -21 \
+-i /flat/s070_m104/raw -t 20 -b -21 \
+-i /flat/s071_m331/raw -t 20 -b -21 \
+-i /flat/s072_m150/raw -t 20 -b -21 \
+-i /flat/s073_m079/raw -t 20 -b -21 \
+-i /flat/s074_m265/raw -t 20 -b -21 \
+-i /flat/s075_m119/raw -t 20 -b -21 \
+-i /flat/s076_m033/raw -t 20 -b -21 \
+-i /flat/s077_m286/raw -t 20 -b -21 \
+-i /flat/s078_m279/raw -t 20 -b -21 \
+-i /flat/s079_m214/raw -t 20 -b -21 \
 --n5TransformGroup ${TRANSFORM_GROUP} \
 --n5PathOutput ${N5_PATH} \
 --n5DatasetOutput ${DATA_SET_OUTPUT} \
@@ -50,24 +53,16 @@ ARGV="\
 # --normalizeContrast
 # --explainPlan        # use --explainPlan option to output debug info without running export
 
-#COMMIT="db085854100f84ec9de178b1ac0ea2ddfa60ab03"
-#HOT_KNIFE_JAR="/groups/flyem/data/render/lib/hot-knife-0.0.4-${COMMIT}-SNAPSHOT.jar"
-HOT_KNIFE_JAR="/groups/flyem/data/render/lib/hot-knife.latest-fat.jar"
 CLASS="org.janelia.saalfeldlab.hotknife.SparkExportAlignedSlabSeries"
 
 # --------------------------------------------------------------------
-# setup spark parameters (11 cores per worker)
+# TODO: need to update this for wafer 53 once we've tried a couple of options ...
 
-export N_EXECUTORS_PER_NODE=2
-export N_CORES_PER_EXECUTOR=5
-export N_OVERHEAD_CORES_PER_WORKER=1
-# Note: N_CORES_PER_WORKER=$(( (N_EXECUTORS_PER_NODE * N_CORES_PER_EXECUTOR) + N_OVERHEAD_CORES_PER_WORKER ))
-
-# To distribute work evenly, recommended number of tasks/partitions is 3 times the number of cores.
+# To distribute work evenly, recommended/default number of tasks/partitions is 3 times the number of cores.
 #export N_TASKS_PER_EXECUTOR_CORE=3
 
-# changed default tasks per core from 3 to 12 based upon ...
-#   with zBatch 1:40, each batch contains 72,216 blocks
+# changed wafer 52 default tasks per core from 3 to 12 based upon ...
+#   with zBatch 1:40, each wafer 52 batch contains 72,216 blocks
 #   total tasks = worker nodes * active cores per worker * tasks per core = 100 nodes * 10 active cores * 12 = 12,000 tasks
 #   72,216 blocks / 12,000 tasks = roughly 6 blocks per task
 #
@@ -75,10 +70,6 @@ export N_OVERHEAD_CORES_PER_WORKER=1
 # setup will also still work for smaller woker node clusters (e.g. a 25 node cluster would have 24 blocks per task)
 # only need to be careful for larger node clusters since you want to ensure multiple blocks per task
 export N_TASKS_PER_EXECUTOR_CORE=12
-
-export N_CORES_DRIVER=1
-
-export LSF_PROJECT="hess"
 
 LOG_FILE=$(setupRunLog "export-aligned-slab-series-batch")
 
