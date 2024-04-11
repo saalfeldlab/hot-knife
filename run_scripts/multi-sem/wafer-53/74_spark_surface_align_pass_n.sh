11beb5ec1aa17696a3b07ecea0af793bc8e81270
Eric Trautman <trautmane@janelia.hhmi.org>
2024-04-10 21:07:22 -0400

    add processing times for 20 slab run

--- run_scripts/multi-sem/wafer-53/74_spark_surface_align_pass_n.sh
+++ run_scripts/multi-sem/wafer-53/74_spark_surface_align_pass_n.sh
@@ -3,7 +3,6 @@
 set -e
 
 # Run times for ten wafer 53 slabs (s070 - s079):
-#
 #  1|2|3)        N_NODES=60
 #    pass01:  7 minutes, pass02:  6 minutes, pass03: 12 minutes
 #  4|5|6|7)      N_NODES=150
@@ -12,6 +11,13 @@ set -e
 #    pass08: 36 minutes, pass09: 42 minutes, pass10: 48 minutes
 #  11|12)        N_NODES=210
 #    pass11: 47 minutes (many h6 nodes), pass12: 83 minutes (many h6 nodes)
+
+# Run times for twenty wafer 53 slabs (s070 - s089):
+#  1|2|3)        N_NODES=60
+#    pass01: 12 minutes, pass02: 10 minutes, pass03: 19 minutes
+#  4|5|6)        N_NODES=150
+#    pass04: 38 minutes, pass05: 59 minutes, pass06: 58 minutes
+
 
 if (( $# < 1 )); then
   echo "USAGE $0 <pass (1-12)> [number of nodes (overrides default)]"
