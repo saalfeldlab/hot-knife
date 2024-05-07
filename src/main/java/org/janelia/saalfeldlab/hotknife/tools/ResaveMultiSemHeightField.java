c071c1d980e7be281b42e2a953211ddb6d0b72da
Eric Trautman <trautmane@janelia.hhmi.org>
2024-05-07 15:49:08 -0400

    restructure ResaveMultiSemHeightField so that it can be reused within a Spark context

--- src/main/java/org/janelia/saalfeldlab/hotknife/tools/ResaveMultiSemHeightField.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/tools/ResaveMultiSemHeightField.java
@@ -73,19 +73,27 @@ public class ResaveMultiSemHeightField implements Callable<Void>{
 
 	@Override
 	public final Void call() throws IOException, InterruptedException, ExecutionException {
+		final ExecutorService service = Executors.newCachedThreadPool();
+		saveSmoothedHeightField(this, service);
+		service.shutdown();
+		return null;
+	}
 
-		final N5Reader sourceN5 = new N5FSReader(n5Path);
-		final String n5OutputPath = (n5Out == null) ? n5Path : n5Out;
+	public static void saveSmoothedHeightField(final ResaveMultiSemHeightField parameters,
+											   final ExecutorService optionalCopyService) throws IOException {
+
+		final N5Reader sourceN5 = new N5FSReader(parameters.n5Path);
+		final String n5OutputPath = (parameters.n5Out == null) ? parameters.n5Path : parameters.n5Out;
 		final N5Writer targetN5 = new N5FSWriter(n5OutputPath);
 
 		// load the min height field and simply resave (check that everything is 0)
-		final String minHeightField = fieldGroup + "/min";
-		final String minHeightFieldOut = fieldGroupOut + "/min";
+		final String minHeightField = parameters.fieldGroup + "/min";
+		final String minHeightFieldOut = parameters.fieldGroupOut + "/min";
 
 		confirmGroupExists(sourceN5, minHeightField);
 		confirmGroupDoesntExist(sourceN5, minHeightFieldOut);
 
-		System.out.println("LOADING height field " + n5Path + minHeightField);
+		System.out.println("LOADING height field " + parameters.n5Path + minHeightField);
 		RandomAccessibleInterval<FloatType> heightFieldSource = N5Utils.open(sourceN5, minHeightField);
 
 		for (final FloatType t : Views.iterable(heightFieldSource)) {
@@ -98,43 +106,43 @@ public class ResaveMultiSemHeightField implements Callable<Void>{
 		System.out.println("SAVING height field " + n5OutputPath + minHeightFieldOut);
 		DatasetAttributes attributes = sourceN5.getDatasetAttributes(minHeightField);
 		N5Utils.save(heightFieldSource, targetN5, minHeightFieldOut, attributes.getBlockSize(), attributes.getCompression());
-		final double avg = sourceN5.getAttribute(minHeightField, "avg", double.class);
-		System.out.println( "Setting attributes avg=" + avg + ", downsamplingFactors=" + Arrays.toString( downsamplingFactors ));
-		targetN5.setAttribute(minHeightFieldOut, "avg", avg);
-		targetN5.setAttribute(minHeightFieldOut, "downsamplingFactors", downsamplingFactors);
+		final double minAvg = sourceN5.getAttribute(minHeightField, "avg", double.class);
+		System.out.println("Setting min attributes avg=" + minAvg + ", downsamplingFactors=" + Arrays.toString(parameters.downsamplingFactors));
+		targetN5.setAttribute(minHeightFieldOut, "avg", minAvg);
+		targetN5.setAttribute(minHeightFieldOut, "downsamplingFactors", parameters.downsamplingFactors);
 
 		// load the max height field, smooth, and resave
-		final String maxHeightField = fieldGroup + "/max";
-		final String maxHeightFieldOut = fieldGroupOut + "/max";
+		final String maxHeightField = parameters.fieldGroup + "/max";
+		final String maxHeightFieldOut = parameters.fieldGroupOut + "/max";
 
 		confirmGroupExists(sourceN5, maxHeightField);
 		confirmGroupDoesntExist(sourceN5, maxHeightFieldOut);
 
-		System.out.println("LOADING height field " + n5Path + maxHeightField);
+		System.out.println("LOADING height field " + parameters.n5Path + maxHeightField);
 		heightFieldSource = N5Utils.open(sourceN5, maxHeightField);
 
-		final ExecutorService service = Executors.newCachedThreadPool();
 		final Img<FloatType> heightField = new ArrayImgFactory<>(new FloatType()).create(heightFieldSource);
-		Util.copy(heightFieldSource, heightField, service);
+		if (optionalCopyService == null) {
+			Util.copy(heightFieldSource, heightField);
+		} else {
+			Util.copy(heightFieldSource, heightField, optionalCopyService);
+		}
 
 		System.out.println("SMOOTHING heightfield");
 		Gauss3.gauss(SIGMA, Views.extendBorder(heightField), heightField);
 
 		System.out.println("SAVING height field " + n5OutputPath + maxHeightFieldOut);
-		final double avg = sourceN5.getAttribute(maxHeightField, "avg", double.class);
+		final double maxAvg = sourceN5.getAttribute(maxHeightField, "avg", double.class);
 		attributes = sourceN5.getDatasetAttributes(maxHeightField);
 
 		N5Utils.save(heightField, targetN5, maxHeightFieldOut, attributes.getBlockSize(), attributes.getCompression());
 
-		System.out.println( "Setting attributes avg=" + avg + ", downsamplingFactors=" + Arrays.toString( downsamplingFactors ));
-		targetN5.setAttribute(maxHeightFieldOut, "avg", avg);
-		targetN5.setAttribute(maxHeightFieldOut, "downsamplingFactors", downsamplingFactors);
+		System.out.println( "Setting max attributes avg=" + maxAvg + ", downsamplingFactors=" + Arrays.toString(parameters.downsamplingFactors));
+		targetN5.setAttribute(maxHeightFieldOut, "avg", maxAvg);
+		targetN5.setAttribute(maxHeightFieldOut, "downsamplingFactors", parameters.downsamplingFactors);
 
-		service.shutdown();
 		sourceN5.close();
 		targetN5.close();
-
-		return null;
 	}
 
 	private static void confirmGroupExists(final N5Reader n5, final String dataset)
