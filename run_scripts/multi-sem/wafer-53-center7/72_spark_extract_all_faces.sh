827db6040b2f4e6415dfbf381292fb00ca468448
Eric Trautman <trautmane@janelia.hhmi.org>
2024-05-09 08:04:22 -0400

    remove split number nodes support for top and bottom faces after fixing min parameter issue that was causing bottom face extraction to run faster

--- run_scripts/multi-sem/wafer-53-center7/72_spark_extract_all_faces.sh
+++ run_scripts/multi-sem/wafer-53-center7/72_spark_extract_all_faces.sh
@@ -3,21 +3,18 @@
 set -e
 
 if (( $# < 2 )); then
-  echo "USAGE $0 <raw slab> <top nodes> <bottom nodes> (e.g. s071_m331 67 30)"
+  echo "USAGE $0 <raw slab> <nodes> (e.g. s071_m331 60)"
   exit 1
 fi
 
 RAW_SLAB="${1}"
 
-# 60 11-core worker nodes: /flat/s071_m331/top4... (7 mFOVs) invert+normalize took 32 min (likely had fast h06 sapphire rapids nodes)
-# 50 11-core worker nodes: /flat/s070_m104/top4... (7 mFOVs) invert took 5 min, invert+normalize took 102 min
-# 35 11-core worker nodes: /flat/s071_m331/top4... (7 mFOVs) invert took 6 min
-TOP_NODES="${2}"
-
-# 60 11-core worker nodes: /flat/s071_m331/bot4... (7 mFOVs) invert+normalize took 58 min
-# 50 11-core worker nodes: /flat/s070_m104/bot4... (7 mFOVs) invert took 4 min, invert+normalize took  92 min
-# 35 11-core worker nodes: /flat/s071_m331/bot4... (7 mFOVs) invert took 6 min
-BOT_NODES="${3}"
+# 60 fast h06 sapphire rapids 11-core worker nodes: /flat/s071_m331/top4... (7 mFOVs) invert+normalize took  32 min
+# 60 11-core worker nodes:                          /flat/s071_m331/bot4... (7 mFOVs) invert+normalize took  58 min
+# 50 11-core worker nodes:                          /flat/s070_m104/top4... (7 mFOVs) invert+normalize took 102 min
+# 50 11-core worker nodes:                          /flat/s070_m104/top4... (7 mFOVs) invert           took   5 min
+# 35 11-core worker nodes:                          /flat/s071_m331/top4... (7 mFOVs) invert           took   6 min
+N_NODES="${2}"
 
 ABSOLUTE_SCRIPT=$(readlink -m "${0}")
 SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
@@ -27,6 +24,6 @@ BOT_SURFACE_DEPTH=4
 SURFACE_SIZE=31 # TODO: remove this hardcoding and read from attributes.json in 72_spark_extract_face.sh
 COLOR="in" # i, n, in (or nothing)
 
-${SCRIPT_DIR}/72_spark_extract_face.sh ${RAW_SLAB} ${TOP_NODES} top ${TOP_SURFACE_DEPTH} ${SURFACE_SIZE} ${COLOR}
+${SCRIPT_DIR}/72_spark_extract_face.sh ${RAW_SLAB} ${N_NODES} top ${TOP_SURFACE_DEPTH} ${SURFACE_SIZE} ${COLOR}
 sleep 5
-${SCRIPT_DIR}/72_spark_extract_face.sh ${RAW_SLAB} ${BOT_NODES} bot ${BOT_SURFACE_DEPTH} ${SURFACE_SIZE} ${COLOR}
+${SCRIPT_DIR}/72_spark_extract_face.sh ${RAW_SLAB} ${N_NODES} bot ${BOT_SURFACE_DEPTH} ${SURFACE_SIZE} ${COLOR}
