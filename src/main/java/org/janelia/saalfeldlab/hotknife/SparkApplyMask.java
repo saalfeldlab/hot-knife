4f9fdee5567ac1b404cc4684b6e3734deaa09a4c
Michael Innerberger <innerbergerm@hhmi.org>
2024-05-16 12:31:41 -0400

    Remove local spark config

--- src/main/java/org/janelia/saalfeldlab/hotknife/SparkApplyMask.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/SparkApplyMask.java
@@ -148,7 +148,7 @@ public class SparkApplyMask {
 		}
 
 		// TODO: this is for local testing; remove for production
-		final SparkConf conf = new SparkConf().setAppName("SparkPixelNormalizeN5").setMaster("local[*]");
+		final SparkConf conf = new SparkConf().setAppName("SparkPixelNormalizeN5");
 		final JavaSparkContext sparkContext = new JavaSparkContext(conf);
 		sparkContext.setLogLevel("ERROR");
 		final List<JavaFutureAction<Void>> futures = new ArrayList<>(paths.size());
