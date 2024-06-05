45bd60f673b794a6f39a2d7bcea60fb2d920314a
Michael Innerberger <innerbergerm@hhmi.org>
2024-06-05 07:31:03 -0400

    Clean up InpaintMultiSEM

--- src/main/java/org/janelia/saalfeldlab/hotknife/tools/InpaintMultiSEM.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/tools/InpaintMultiSEM.java
@@ -27,36 +27,45 @@ import net.imglib2.util.Intervals;
 import net.imglib2.util.LinAlgHelpers;
 import net.imglib2.view.Views;
 
+
+/**
+ * Inpaints missing values in a 2D/3D image. This was used for the wafer 53 multi-SEM data set, since there were missing
+ * regions in the data due to errors in the MFOV positioning during acquisition.
+ *
+ * @author Stephan Preibisch
+ * @author Michael Innerberger
+ */
 public class InpaintMultiSEM {
 
-	public static final int MAX_INPAINTING_DIAMETER = 100;
+	private static final int MAX_INPAINTING_DIAMETER = 100;
+	private static final int NUM_ORIENTATIONS = 256;
+	private static final double RATIO_OF_2D_RAYS = 0.1;
 
-	public static boolean isInside(final RealLocalizable p, final Interval r)
-	{
-		for ( int d = 0; d < p.numDimensions(); ++d )
-		{
-			final double l = p.getDoublePosition( d );
+	private static boolean isInside(final RealLocalizable p, final Interval r) {
+		for (int d = 0; d < p.numDimensions(); ++d) {
+			final double l = p.getDoublePosition(d);
 
-			if ( l < r.min( d ) || l > r.max( d ) )
+			if (l < r.min(d) || l > r.max(d))
 				return false;
 		}
 
 		return true;
 	}
 
-	public static void inpaint3d( final RandomAccessibleInterval< FloatType > img, final RandomAccessibleInterval< FloatType > out )
-	{
-		final Cursor<FloatType> c = Views.iterable( img ).localizingCursor();
-		final RandomAccess< FloatType > o = out.randomAccess();
+	/**
+	 * Inpaints missing values in a 2D/3D image by casting rays in random directions and averaging the values of the
+	 * first non-masked pixel.
+	 *
+	 * @param img the image to inpaint
+	 * @param out the output image
+	 */
+	public static void inpaint3d(final RandomAccessibleInterval<FloatType> img, final RandomAccessibleInterval<FloatType> out) {
+		final Cursor<FloatType> c = Views.iterable(img).localizingCursor();
+		final RandomAccess<FloatType> o = out.randomAccess();
 
-		// random rays being shot out
-		final int numOrientations = 256;
-		final double p2d = 0.1;
-		final Random rnd = new Random( 89656 );
-
-		final RealRandomAccess< FloatType > iR = Views.interpolate( Views.extendBorder( img ), new NLinearInterpolatorFactory<>() ).realRandomAccess();
-
-		final RayCaster rayCaster = new RayCaster(iR, p2d, rnd, MAX_INPAINTING_DIAMETER);
+		final Random rnd = new Random(89656);
+		final RealRandomAccess<FloatType> iR = Views.interpolate(Views.extendBorder(img), new NLinearInterpolatorFactory<>()).realRandomAccess();
+		final RayCaster rayCaster = new RayCaster(iR, RATIO_OF_2D_RAYS, rnd, MAX_INPAINTING_DIAMETER);
 
 		while (c.hasNext()) {
 			final FloatType v = c.next();
@@ -75,7 +84,7 @@ public class InpaintMultiSEM {
 
 				// interpolate value by casting rays in random directions and averaging (weighted by distances) the
 				// values of the first non-masked pixel
-				while (nRays < numOrientations) {
+				while (nRays < NUM_ORIENTATIONS) {
 					nRays++;
 					final RayCaster.Result result = rayCaster.cast(img, c, RayCaster.Direction.RANDOM);
 					if (result != null) {
@@ -91,8 +100,7 @@ public class InpaintMultiSEM {
 		}
 	}
 
-	public static void main( String[] args )
-	{
+	public static void main(String[] args) {
 		final String n5Path = "/nrs/hess/data/hess_wafer_53/export/hess_wafer_53_center7.n5";
 		final String maskDataset = "/render/slab_000_to_009/s006_m167_align_no35_horiz_avgshd_ic___mask_20240504_144727/s0";
 		final String imageDataset = "/render/slab_000_to_009/s006_m167_align_no35_horiz_avgshd_ic___20240504_085007_norm-layer-clahe/s0";
@@ -103,23 +111,23 @@ public class InpaintMultiSEM {
 
 		//final Interval interval = Intervals.createMinMax( 32313, 37000, 12, 33808, 37800, 16 );
 		//final Interval interval = Intervals.createMinMax( 32313, 37000, 14, 33808, 37800, 14 );
-		final Interval interval = Intervals.createMinMax(36000, 42600, 14, 36600, 44100, 14);
+		final Interval interval = Intervals.createMinMax(36000, 42600, 12, 36600, 44100, 16);
 		//final Interval interval = Intervals.createMinMax( 32313, 37000, 14, 32540, 37194, 14 );
 
 		new ImageJ();
 
 		System.out.println("Showing images...");
-		final RandomAccessibleInterval<UnsignedByteType> img = Views.interval( imgRaw , interval );
-		final RandomAccessibleInterval<UnsignedByteType> mask = Views.interval( maskRaw , interval );
+		final RandomAccessibleInterval<UnsignedByteType> img = Views.interval(imgRaw , interval);
+		final RandomAccessibleInterval<UnsignedByteType> mask = Views.interval(maskRaw , interval);
 
-		ImageJFunctions.show( img );
-		ImageJFunctions.show( mask );
+		ImageJFunctions.show(img);
+		ImageJFunctions.show(mask);
 
 		System.out.println("Converting to float...");
-		final RandomAccessibleInterval< FloatType > imgF = Views.translate( ArrayImgs.floats( img.dimensionsAsLongArray() ), img.minAsLongArray() );
-		Util.copy( Converters.convert( img, (a,b) -> b.set( a.get() ), new FloatType() ), imgF );
+		final RandomAccessibleInterval<FloatType> imgF = Views.translate(ArrayImgs.floats(img.dimensionsAsLongArray()), img.minAsLongArray());
+		Util.copy( Converters.convert(img, (a,b) -> b.set(a.get()), new FloatType()), imgF);
 
-		final RandomAccessibleInterval< FloatType > imgOut = Views.translate( ArrayImgs.floats( img.dimensionsAsLongArray() ), img.minAsLongArray() );
+		final RandomAccessibleInterval<FloatType> imgOut = Views.translate(ArrayImgs.floats(img.dimensionsAsLongArray()), img.minAsLongArray());
 
 		System.out.println("Mask slice...");
 		SparkSurfaceFit.maskSlice(imgF, mask, new FloatType(Float.NaN));
@@ -129,8 +137,8 @@ public class InpaintMultiSEM {
 		inpaint3d( Views.zeroMin( imgF ), Views.zeroMin( imgOut ));
 		System.out.println("Inpainting took " + (System.currentTimeMillis() - start) + "ms");
 
-		ImageJFunctions.show( imgF );
-		ImageJFunctions.show( imgOut );
+		ImageJFunctions.show(imgF);
+		ImageJFunctions.show(imgOut);
 	}
 
 
@@ -143,7 +151,7 @@ public class InpaintMultiSEM {
 		private final Random random;
 		private final long maxRayLength;
 
-		/*
+		/**
 		 * @param image the image to cast rays into
 		 * @param ratioOf2dRays the ratio of 2d rays to 3d rays. If the number of dimensions is less than 3, this
 		 * 		  parameter is ignored.
@@ -159,13 +167,13 @@ public class InpaintMultiSEM {
 			this.maxRayLength = maxRayLength;
 		}
 
-		/*
+		/**
 		 * Casts a ray from the given position in the given direction until it hits a non-masked (i.e., non-NaN) pixel
 		 * or exits the image boundary.
 		 *
 		 * @param interval the interval in which the ray search is performed
 		 * @param position the position from which to cast the ray
-		 * @param direction the direction in which to cast the ray
+		 * @param rayDirection the direction in which to cast the ray
 		 * @return the result of the ray casting or null if the ray exited the image boundary without hitting a
 		 * 		   non-NaN pixel
 		 */
@@ -219,7 +227,7 @@ public class InpaintMultiSEM {
 			direction[2] = rayDirection.get()[2];
 		}
 
-		/*
+		/**
 		 * Checks if the given position is in the pseudo-convex hull of the image content. The pseudo-convex hull is
 		 * defined as the set of all points from which at both rays in +/-x or +/-y directions hit the image
 		 * content. This is taking advantage of the fact that the image content is made up from multiple almost
