435a6fa49c4cae97950835bda014027c58d25132
Stephan Preibisch <stephan.preibisch@gmx.de>
2024-11-23 10:03:35 -0500

    N5PathSupplier now uses N5Factory to instantiate N5Writer

--- src/main/java/org/janelia/saalfeldlab/hotknife/util/N5PathSupplier.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/util/N5PathSupplier.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.spark.supplier.N5WriterSupplier;
+import org.janelia.saalfeldlab.n5.universe.N5Factory;
+import org.janelia.saalfeldlab.n5.universe.N5Factory.StorageFormat;
 
 /**
  * Serializable downsample supplier for spark.
@@ -19,7 +21,7 @@ public class N5PathSupplier implements N5WriterSupplier {
     @Override
     public N5Writer get()
             throws IOException {
-        return new N5FSWriter(path);
+        return new N5Factory().openWriter( StorageFormat.N5, path );//new N5FSWriter(path);
     }
 }
 
