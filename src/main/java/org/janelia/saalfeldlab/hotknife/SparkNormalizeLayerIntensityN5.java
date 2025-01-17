9cecd7ad112d76e94ecc4cd415e37c9663fde37a
Michael Innerberger <innerbergerm@hhmi.org>
2025-01-17 16:47:00 -0500

    Prepare SparkNormalizeLayerIntensityN5 for generifying

--- src/main/java/org/janelia/saalfeldlab/hotknife/SparkNormalizeLayerIntensityN5.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/SparkNormalizeLayerIntensityN5.java
@@ -12,6 +12,8 @@ import net.imglib2.converter.Converters;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.loops.LoopBuilder;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.NumericType;
 import net.imglib2.view.IntervalView;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -38,23 +40,39 @@ import net.imglib2.view.Views;
 import static org.janelia.saalfeldlab.hotknife.AbstractOptions.parseCSIntArray;
 import static org.janelia.saalfeldlab.n5.spark.downsample.scalepyramid.N5ScalePyramidSpark.downsampleScalePyramid;
 
-public class SparkNormalizeLayerIntensityN5 {
+
+/**
+ * Normalize layer intensity in N5 data set. The normalization is done by shifting the intensity of each layer relative
+ * to the first layer. The shifts are computed based on the average intensity of a column of pixels that have "content"
+ * throughout the stack (i.e. pixels that are not background).
+ *
+ * @param <T> pixel type; determined automatically from the input stack
+ */
+public class SparkNormalizeLayerIntensityN5<T extends NativeType<T> & NumericType<T>> {
 
 	@SuppressWarnings({"FieldMayBeFinal", "unused"})
 	public static class Options extends AbstractOptions implements Serializable {
 
-		@Option(name = "--n5PathInput",
+		@Option(name = "--n5Path",
 				required = true,
-				usage = "Input N5 path, e.g. /nrs/hess/data/hess_wafer_53/export/hess_wafer_53b.n5")
-		private String n5PathInput = null;
+				usage = "N5 path, e.g. /nrs/hess/data/hess_wafer_53/export/hess_wafer_53b.n5")
+		private String n5Path = null;
 
 		@Option(name = "--n5DatasetInput",
 				required = true,
 				usage = "Input N5 dataset, e.g. /render/slab_070_to_079/s075_m119_align_big_block_ic___20240308_072106")
 		private String n5DatasetInput = null;
 
-		@Option(
-				name = "--factors",
+		@Option(name = "--n5DatasetOutput",
+				required = true,
+				usage = "Output N5 dataset, e.g. /render/slab_070_to_079/s075_m119_align_big_block_ic___20240308_072106_norm-layer")
+		private String n5DatasetOutput = null;
+
+		@Option(name = "--downsampleLevel",
+				usage = "Take this downsample level for computing the intensity shifts.")
+		private Integer downsampleLevel = 5;
+
+		@Option(name = "--factors",
 				usage = "If specified, generates a scale pyramid with given factors, e.g. 2,2,1")
 		public String factors;
 
@@ -77,57 +95,49 @@ public class SparkNormalizeLayerIntensityN5 {
 			throw new IllegalArgumentException("Options were not parsed successfully");
 		}
 
-		final SparkConf conf = new SparkConf().setAppName("SparkNormalizeLayerIntensityN5");
-		final JavaSparkContext sparkContext = new JavaSparkContext(conf);
-		sparkContext.setLogLevel("ERROR");
+		final N5Reader n5reader = new N5FSReader(options.n5Path);
 
-		final N5Reader n5Input = new N5FSReader(options.n5PathInput);
+		if (n5reader.exists(options.n5DatasetOutput)) {
+			throw new IllegalArgumentException("Normalized data set already exists: " + options.n5DatasetOutput);
+		}
 
 		final String fullScaleInputDataset = options.n5DatasetInput + "/s0";
-		final int[] blockSize = n5Input.getAttribute(fullScaleInputDataset, "blockSize", int[].class);
-		final long[] dimensions = n5Input.getAttribute(fullScaleInputDataset, "dimensions", long[].class);
+		final DatasetAttributes attributes = n5reader.getDatasetAttributes(fullScaleInputDataset);
 
-		final int[] gridBlockSize = new int[] { blockSize[0] * 8, blockSize[1] * 8, blockSize[2] };
-		final List<long[][]> grid = Grid.create(dimensions, gridBlockSize, blockSize);
+		final List<long[][]> grid = Grid.create(attributes.getDimensions(), attributes.getBlockSize());
 
-		final N5Writer n5Output = new N5FSWriter(options.n5PathInput);
-		final String outputDataset = options.n5DatasetInput + "_norm-layer";
-		final String fullScaleOutputDataset = outputDataset + "/s0";
+		final N5Writer n5Writer = new N5FSWriter(options.n5Path);
+		final String fullScaleOutputDataset = options.n5DatasetOutput + "/s0";
 
-		final String downScaledDataset = options.n5DatasetInput + "/s5";
-		final Img<UnsignedByteType> downScaledImg = N5Utils.open(n5Input, downScaledDataset);
+		final String downScaledDataset = options.n5DatasetInput + "/s" + options.downsampleLevel;
+		final Img<UnsignedByteType> downScaledImg = N5Utils.open(n5reader, downScaledDataset);
 		final List<Double> shifts = computeShifts(downScaledImg);
 
-		if (n5Output.exists(fullScaleOutputDataset)) {
-			final String fullPath = options.n5PathInput + fullScaleOutputDataset;
-			throw new IllegalArgumentException("Normalized data set exists: " + fullPath);
+		n5Writer.createDataset(fullScaleOutputDataset, attributes);
+
+		final SparkConf conf = new SparkConf().setAppName("SparkNormalizeLayerIntensityN5");
+		try (final JavaSparkContext sparkContext = new JavaSparkContext(conf)) {
+
+			final JavaRDD<long[][]> pGrid = sparkContext.parallelize(grid);
+			pGrid.foreach(gridBlock -> saveFullScaleBlock(options.n5Path,
+														  fullScaleInputDataset,
+														  fullScaleOutputDataset,
+														  shifts,
+														  attributes.getDimensions(),
+														  attributes.getBlockSize(),
+														  gridBlock));
+			n5Writer.close();
+			n5reader.close();
+
+			final int[] downsampleFactors = parseCSIntArray(options.factors);
+			if (downsampleFactors != null) {
+				downsampleScalePyramid(sparkContext,
+									   new N5PathSupplier(options.n5Path),
+									   fullScaleOutputDataset,
+									   options.n5DatasetOutput,
+									   downsampleFactors);
+			}
 		}
-
-		n5Output.createDataset(fullScaleOutputDataset, dimensions, blockSize, DataType.UINT8, new GzipCompression());
-
-		final JavaRDD<long[][]> pGrid = sparkContext.parallelize(grid);
-		pGrid.foreach(
-				gridBlock -> saveFullScaleBlock(options.n5PathInput,
-												options.n5PathInput,
-												fullScaleInputDataset,
-												fullScaleOutputDataset,
-												shifts,
-												dimensions,
-												blockSize,
-												gridBlock ));
-		n5Output.close();
-		n5Input.close();
-
-		final int[] downsampleFactors = parseCSIntArray(options.factors);
-		if (downsampleFactors != null) {
-			downsampleScalePyramid(sparkContext,
-								   new N5PathSupplier(options.n5PathInput),
-								   fullScaleOutputDataset,
-								   outputDataset,
-								   downsampleFactors);
-		}
-
-		sparkContext.close();
 	}
 
 
@@ -210,8 +220,7 @@ public class SparkNormalizeLayerIntensityN5 {
 		return stack;
 	}
 
-	private static void saveFullScaleBlock(final String n5PathInput,
-										   final String n5PathOutput,
+	private static void saveFullScaleBlock(final String n5Path,
 										   final String datasetName, // should be s0
 										   final String datasetNameOutput,
 										   final List<Double> shifts,
@@ -219,10 +228,9 @@ public class SparkNormalizeLayerIntensityN5 {
 										   final int[] blockSize,
 										   final long[][] gridBlock ) {
 
-		final N5Reader n5Input = new N5FSReader(n5PathInput);
-		final N5Writer n5Output = new N5FSWriter(n5PathOutput);
+		final N5Writer n5Writer = new N5FSWriter(n5Path);
 
-		final RandomAccessibleInterval<UnsignedByteType> sourceRaw = N5Utils.open(n5Input, datasetName);
+		final RandomAccessibleInterval<UnsignedByteType> sourceRaw = N5Utils.open(n5Writer, datasetName);
 
 		final RandomAccessibleInterval<UnsignedByteType> filteredSource = applyShifts(sourceRaw, shifts);
 
@@ -231,7 +239,7 @@ public class SparkNormalizeLayerIntensityN5 {
 										gridBlock[1][0], gridBlock[1][1], gridBlock[1][2]);
 
 		N5Utils.saveNonEmptyBlock(Views.interval(filteredSource, gridBlockInterval),
-								  n5Output,
+								  n5Writer,
 								  datasetNameOutput,
 								  new DatasetAttributes(dimensions, blockSize, DataType.UINT8, new GzipCompression()),
 								  gridBlock[2],
