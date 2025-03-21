657b4705011ef77f80f1c291ed44ffd5466c8171
Stephan Preibisch <stephan.preibisch@gmx.de>
2025-03-20 21:08:36 -0400

    undo SNAPSHOT for imglib2-realtransform, issue resolved

--- src/main/java/org/janelia/saalfeldlab/hotknife/tools/TestFieldLoading.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/tools/TestFieldLoading.java
@@ -49,12 +49,14 @@ public class TestFieldLoading
 		final double transformScale = n5.getAttribute(group + "/" + dataset, "scale", double.class);
 		final RandomAccessibleInterval<DoubleType> positionField = N5Utils.open(n5, group + "/" + dataset);
 
-		// TODO: copy into an ArrayImg 
+		// note: transformation field for pass00-sofima is different by a constant 128 in the X plane
+		// (identical to the result of the on the fly modification below)
 		//ImageJFunctions.show( positionField ).setTitle( group );
 
-		// on the fly modification
-		//IntervalView<DoubleType> positionFieldScaledX = Views.hyperSlice( positionField, 2, 0 );
-		//Views.iterable( positionField ).forEach( v -> v.set( v.get() + 128 ) );
+		// TODO: copy into an ArrayImg 
+		// on the fly modification (this is a hack, only works if the cached cells are kept in memory) 
+		//IntervalView<DoubleType> positionFieldX = Views.hyperSlice( positionField, 2, 0 );
+		//Views.iterable( positionFieldX ).forEach( v -> v.set( v.get() + 128 ) );
 
 		final int n = positionField.numDimensions() - 1;
 		final long[] translation = Arrays.copyOf(Grid.floorScaled(boundsMin, transformScale), n + 1);
@@ -72,7 +74,6 @@ public class TestFieldLoading
 
 		// sofima: [35301.037084840544, 59427.077371588675]
 		// pass00: [31205.037084840547, 59427.077371588675]
-
 		System.out.println( Arrays.toString( l1 ) + " maps to: " + Arrays.toString( l2 ) );
 
 		final int scaleIndex = 4;
@@ -82,14 +83,19 @@ public class TestFieldLoading
 						n5, "/flat_mask/s001_m239/bot4_clahe/face/s" + scaleIndex),
 						(in,o) -> o.set( Math.round( in.get() )), new UnsignedByteType() );
 
-		System.out.println( "source: " + Util.printInterval( source ) );
+		// note: sources are identical for both cases
+		//ImageJFunctions.show( source ).setTitle( "source " + group );
+
 		// sofima: source: [0, 0] -> [3315, 3261], dimensions (3316, 3262)
 		// pass00: source: [0, 0] -> [3315, 3261], dimensions (3316, 3262)
+		System.out.println( "source: " + Util.printInterval( source ) );
 
 		final Interval interval = new FinalInterval(
 				Grid.floorScaled(boundsMin, showScale),
 				Grid.ceilScaled(boundsMax, showScale));
 
+		// sofima: [-690, -724] -> [4145, 4002], dimensions (4836, 4727)
+		// pass00: [-690, -724] -> [4145, 4002], dimensions (4836, 4727)
 		System.out.println( "interval: " + Util.printInterval( interval ) );
 
 		RandomAccessibleInterval<UnsignedByteType> rai = Transform.createTransformedInterval(
@@ -98,19 +104,27 @@ public class TestFieldLoading
 				Transform.createScaledRealTransform(transform, scaleIndex),
 				new UnsignedByteType(255));
 
+		// sofima: [-690, -724] -> [4145, 4002], dimensions (4836, 4727)
+		// pass00: [-690, -724] -> [4145, 4002], dimensions (4836, 4727)
 		System.out.println( "rai: " + Util.printInterval( rai ) );
 
-		// TODO: what the hack is going on when I now make a randomaccess???
+		// so, what the hack is going on when I now make a randomaccess???
 		//RandomAccess<UnsignedByteType> ra1 = rai.randomAccess();
 		RealTransformRandomAccessible<UnsignedByteType, ?>.RealTransformRandomAccess ra =
 				(RealTransformRandomAccessible<UnsignedByteType, ?>.RealTransformRandomAccess)rai.randomAccess();
 
-		RealTransformRandomAccessible.debug = true;
+		// TODO: solution, we need to pre-concatenate!!! (i.e. transform the constant 128 offset)
 		ra.setPosition( new long[] { 3120 + rai.min( 0 ), 1920 + rai.min( 1 ) } );
+
+		// note: apply(); in RealTransformRandomAccess.get() gives the same value in both cases
+		// sofima: [2430.0, 1196.0] >> [1054.2288091916444, 1477.0132572053976]
+		// pass00: [2430.0, 1196.0] >> [798.2288091916444, 1477.0132572053976]
 		UnsignedByteType v = ra.get();
 		int value = v.get();
+
+		// sofima: Pixel value at [3120, 1920]: 55
+		// pass00: Pixel value at [3120, 1920]: 159
 		System.out.println( "Pixel value at [3120, 1920]: " + value );
-		RealTransformRandomAccessible.debug = false;
 
 		ImageJFunctions.show( rai, Executors.newFixedThreadPool( 36 ) );
 	}
