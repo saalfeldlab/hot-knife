a2b889200d3e8f3171e0d2c3a6344131293d2409
Eric Trautman <trautmane@janelia.hhmi.org>
2025-12-16 16:38:43 -0500

    add processing time info

--- run_scripts/multi-sem/wafer-60-61/71_export_flat_batch.sh
+++ run_scripts/multi-sem/wafer-60-61/71_export_flat_batch.sh
@@ -13,7 +13,9 @@ Examples:
   $0  40  w61  r00  81 82
 
 Notes:
+  - with 20 max-executors, w61 r00 83    took ?? hours ?? minutes to complete
   - with 40 max-executors, w61 r00 79    took 10 hours 50 minutes to complete
+  - with 50 max-executors, w61 r00 79    took  9 hours 44 minutes to complete
 "
   exit 1
 fi
