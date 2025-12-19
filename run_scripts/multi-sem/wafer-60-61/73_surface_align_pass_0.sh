329a5bd57f06204fb52bd34b28b410ef990c38ae
Eric Trautman <trautmane@janelia.hhmi.org>
2025-12-19 11:14:59 -0500

    update surface root directory

--- run_scripts/multi-sem/wafer-60-61/73_surface_align_pass_0.sh
+++ run_scripts/multi-sem/wafer-60-61/73_surface_align_pass_0.sh
@@ -30,7 +30,7 @@ WAFER="${2}"
 REGION="${3}"
 
 N5_PATH="gs://janelia-spark-test/hess_wafers_60_61_export"
-N5_SURFACE_ROOT="surface-align/run_20251218_210000"
+N5_SURFACE_ROOT="surface-align/run_20251219_110000"
 N5_GROUP_OUTPUT="${N5_SURFACE_ROOT}/pass00"
 
 # need scaleIndex=5 for larger wafer 53 slabs
