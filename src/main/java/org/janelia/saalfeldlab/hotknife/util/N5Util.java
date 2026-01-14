f355b2c9b836835ad6e782c6924273a8a1f4632b
Eric Trautman <trautmane@janelia.hhmi.org>
2026-01-14 10:39:47 -0500

    add verifyDatasetAndAttributesExist utility method

--- src/main/java/org/janelia/saalfeldlab/hotknife/util/N5Util.java
+++ src/main/java/org/janelia/saalfeldlab/hotknife/util/N5Util.java
@@ -1,5 +1,8 @@
 package org.janelia.saalfeldlab.hotknife.util;
 
+import java.io.IOException;
+
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.universe.N5Factory;
@@ -12,5 +15,16 @@ public class N5Util {
 
     public static N5Writer createN5Writer(final String n5Path) {
         return new N5Factory().openWriter(N5Factory.StorageFormat.N5, n5Path);
+    }
+
+    public static void verifyDatasetAndAttributesExist(final N5Reader n5Reader,
+                                                       final String dataset) throws IOException {
+        if (! n5Reader.datasetExists(dataset)) {
+            throw new IOException("dataset '" + dataset + "' does not exist within " + n5Reader.getURI());
+        }
+        final DatasetAttributes attributes = n5Reader.getDatasetAttributes(dataset);
+        if (attributes == null) {
+            throw new IOException("attributes missing for dataset '" + dataset + "' within " + n5Reader.getURI());
+        }
     }
 }
