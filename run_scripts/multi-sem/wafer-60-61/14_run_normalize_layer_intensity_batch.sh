32b0d4c007982d1cfb2c11c22b5bb07974c2e49c
Eric Trautman <trautmane@janelia.hhmi.org>
2026-02-20 08:46:57 -0500

    improve 14_run_normalize_layer_intensity_batch.sh

--- run_scripts/multi-sem/wafer-60-61/14_run_normalize_layer_intensity_batch.sh
+++ run_scripts/multi-sem/wafer-60-61/14_run_normalize_layer_intensity_batch.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
 
-# Utility script to simplify submission of multiple normalize layer intensity jobs.
-#
-# Copy this script locally (e.g. to export-batch.sh) so that STACK_NAMES_WITH_MAX_Z can be filled in
-# with details from running list-stacks.sh on VM
-# and MAX_EXECUTORS and LAUNCH_JOBS values can be set.
-
 set -e
 
-MAX_EXECUTORS=10       # 10 executors for w61_s??? pixel with ?? z layers took ? hours, ?? minutes
-LAUNCH_JOBS="n"        # set to "y" to launch jobs, anything else to just print commands
+if (( $# < 3 )); then
+  echo """
+USAGE: $0 <max executors> <launch jobs y|n> <stack number> [stack number] ...
 
-# STACK_NAMES_WITH_MAX_Z examples:
-#   w61_s080_r00_gc_par_align_ic2d with maxZ 89
-#   w61_s095_r00_gc_par_align_ic2d with maxZ 82
-STACK_NAMES_WITH_MAX_Z="
-"
+Examples:
+  $0  10  n  70 71
+  $0  40  y  79
+  $0  10  y  80 81 82 83 84 85 86 87 88 89
 
-while read -r LINE; do
-    [[ -z "${LINE}" ]] && continue
+With 40 max-executors, s079 took 61 minutes.
+"""
+  exit 1
+fi
 
-    if [[ ${LINE} =~ ^[[:space:]]*([a-zA-Z0-9]+)_s([0-9]{3})_([^ ]+).*maxZ[[:space:]]+([0-9]+) ]]; then
+MAX_EXECUTORS="${1}"
+LAUNCH_JOBS="${2}"
+shift 2
 
-        WAFER=${BASH_REMATCH[1]}                                            # w61
-        SERIAL_STRING=${BASH_REMATCH[2]}                                    # 080
-        STACK="${BASH_REMATCH[1]}_s${BASH_REMATCH[2]}_${BASH_REMATCH[3]}"
+WAFER="w61"
+REGION="r00"
 
-        # ensure decimal math
-        SERIAL_NUM=$((10#${SERIAL_STRING}))
+# shellcheck disable=SC2048
+for STACK_NUMBER in $*; do
 
-        # compute 10-range
-        START=$(( (SERIAL_NUM / 10) * 10 ))
-        END=$(( START + 9 ))
+  if ! [[ "${STACK_NUMBER}" =~ ^[0-9]+$ ]]; then
+      echo "Error: '${STACK_NUMBER}' is not a valid stack number."
+      exit 1
+  fi
 
-        # zero-pad
-        START=$(printf "%03d" "${START}")
-        END=$(printf "%03d" "${END}")
+  # 79 -> 079
+  PADDED_STACK_NUMBER=$(printf "%03d" "${STACK_NUMBER}")
 
-        PROJECT="${WAFER}_serial_${START}_to_${END}"
+  # 079 -> w61_s079_r00
+  STACK="${WAFER}_s${PADDED_STACK_NUMBER}_${REGION}"
 
-        # <number of nodes> <render project> <raw stack>
-        CMD="./13_normalize_layer_intensity.sh ${MAX_EXECUTORS} ${PROJECT} ${STACK}"
+  # w61_s079_r00 -> w61_serial_070_to_079
+  PROJECT=$(awk -F'[_s]' '{w=$1; s=$3+0; lo=int(s/10)*10; hi=lo+9; printf "%s_serial_%03d_to_%03d", w, lo, hi}' <<<"${STACK}")
 
-        if [[ "${LAUNCH_JOBS}" == "y" ]]; then
-          echo
-          echo "Running the following in 10 seconds:"
-          echo "  ${CMD}"
-          echo
-          sleep 10
-          ${CMD}
-        else
-          echo "${CMD}"
-        fi
-    fi
+  CMD="./13_normalize_layer_intensity.sh ${MAX_EXECUTORS} ${PROJECT} ${STACK}"
 
-done <<< "${STACK_NAMES_WITH_MAX_Z}"
+  if [[ "${LAUNCH_JOBS}" == "y" ]]; then
+    echo
+    echo "Running the following in 10 seconds:"
+    echo "  ${CMD}"
+    echo
+    sleep 10
+    ${CMD}
+  else
+    echo "${CMD}"
+  fi
+
+done
